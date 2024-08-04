dbfbc467de2f826348f1cd91bbffa9e2f7693753
LaryssaGabi <lary.gabriela85@gmail.com>
2024-08-03 22:01:31 -0300

    errors

--- backend/src/controllers/categories.controller.ts
+++ backend/src/controllers/categories.controller.ts
@@ -1,17 +1,21 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import { CategoriesService } from "../services/categories.services";
 import { CategoriesRepository } from "../database/repositories/categories.repository";
 import { CategoryModel } from "../database/schemas/category.schema";
 import { CreateCategoryDTO } from "../dtos/categories.dtos";
 
 export class CategoriesController {
-    async create(req: Request<unknown, unknown, CreateCategoryDTO>, res: Response) {
-        const { title, color } = req.body
-        const repository = new CategoriesRepository(CategoryModel)
-        const service = new CategoriesService(repository);
+    async create(req: Request<unknown, unknown, CreateCategoryDTO>, res: Response, next:NextFunction,) {
+        try {
+            const { title, color } = req.body
+            const repository = new CategoriesRepository(CategoryModel)
+            const service = new CategoriesService(repository);
 
-        const result = await service.create({ title, color })
+            const result = await service.create({ title, color })
 
-        return res.status(201).json(result)
+            return res.status(201).json(result)
+        } catch(err){
+            next(err)
+        }
     }
 }
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,13 +2,16 @@ import 'dotenv/config'
 import express, { json } from "express";
 import { routes } from "./routes";
 import { setupMongo } from "./database";
+import { errorHandle } from './middlewares/error-handler.middleware';
+
 
 setupMongo().then(() => {
   const app = express();
   const port = 3333;
 
   app.use(json());
-  app.use(routes)
+  app.use(routes);
+  app.use(errorHandle)
 
   app.listen(port, () => {
     console.log(`🚀 App is running at port ${port}!`);
