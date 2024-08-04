9a41c7324be8c6cf797a1a55b7ddb56430393e4d
LaryssaGabi <lary.gabriela85@gmail.com>
2024-08-04 02:05:42 -0300

    route get categories

---
+++ backend/src/factories/categories.factory.ts
@@ -0,0 +1,16 @@
+import { CategoriesRepository } from "../database/repositories/categories.repository";
+import { CategoryModel } from "../database/schemas/category.schema";
+import { CategoriesService } from "../services/categories.services";
+
+export class CategoriesFactory {
+    private static categoriesService: CategoriesService;
+
+    static getServiceInstance() {
+        if (!this.categoriesService) {
+            const repository = new CategoriesRepository(CategoryModel);
+            const service = new CategoriesService(repository);
+            this.categoriesService = service;
+        }
+        return this.categoriesService;
+    }
+}
--- backend/src/routes/categories.routes.ts
+++ backend/src/routes/categories.routes.ts
@@ -2,10 +2,12 @@ import { Router } from "express";
 import { CategoriesController } from "../controllers/categories.controller";
 import { ParamsType, validator } from "../middlewares/validator.middleware";
 import { createCategorySchema } from "../dtos/categories.dtos";
+import { CategoriesFactory } from "../factories/categories.factory";
 
 export const categoriesRoutes = Router();
 
-const controller = new CategoriesController();
+const controller = new CategoriesController(CategoriesFactory.getServiceInstance());
+
 categoriesRoutes.get('/', controller.index);
 
 categoriesRoutes.post('/',
