3ec675f2f3bf657e3337d305b0b933e5c2da1e7d
LaryssaGabi <lary.gabriela85@gmail.com>
2024-08-03 23:44:18 -0300

    validate

--- backend/src/controllers/categories.controller.ts
+++ backend/src/controllers/categories.controller.ts
@@ -3,17 +3,22 @@ import { CategoriesService } from "../services/categories.services";
 import { CategoriesRepository } from "../database/repositories/categories.repository";
 import { CategoryModel } from "../database/schemas/category.schema";
 import { CreateCategoryDTO } from "../dtos/categories.dtos";
+import { z } from "zod";
+import { StatusCodes } from "http-status-codes";
 
 export class CategoriesController {
     async create(req: Request<unknown, unknown, CreateCategoryDTO>, res: Response, next:NextFunction,) {
         try {
+     
+      
+
             const { title, color } = req.body
             const repository = new CategoriesRepository(CategoryModel)
             const service = new CategoriesService(repository);
 
             const result = await service.create({ title, color })
 
-            return res.status(201).json(result)
+            return res.status(StatusCodes.CREATED).json(result)
         } catch(err){
             next(err)
         }
--- backend/src/errors/app.error.ts
+++ backend/src/errors/app.error.ts
@@ -1,11 +1,13 @@
 import { StatusCodes } from "http-status-codes";
+import { string } from "zod";
 
-export class AppError extends Error {
+export class AppError {
     public statusCode: StatusCodes;
+    public message: string | string[]
 
-    constructor(message: string, statusCode: StatusCodes) {
-        super(message);
+    constructor(message: string | string[], statusCode: StatusCodes) {
 
+        this.message = message
         this.statusCode = statusCode;
     }
 
