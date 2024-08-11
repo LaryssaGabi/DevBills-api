827ce3ed4b82cc3a4442d6a8bfae9c77aefbce2a
LaryssaGabi <lary.gabriela85@gmail.com>
2024-08-10 23:31:04 -0300

    titl

--- frontend/src/screens/home/index.tsx
+++ frontend/src/screens/home/index.tsx
@@ -1,15 +1,25 @@
-import { Header } from "./home-styles";
+import { Filters, Header, Main, Section } from "./home-styles";
 import { Logo } from '../../components/logo/index'
 import { Button } from "../../components/button";
+import { Title } from "../../components/title";
 
 export function Home() {
     return (
-        <Header>
-            <Logo />
-            <div>
-                <Button>Nova transação</Button>
-                <Button>Nova categoria</Button>
-            </div>
-        </Header>
+        <>
+            <Header>
+                <Logo />
+                <div>
+                    <Button>Nova transação</Button>
+                    <Button>Nova categoria</Button>
+                </div>
+            </Header>
+            <Main>
+                <Section>
+                    <Filters>
+                        <Title title="Saldo" subtitle="Receitas e despesas no período" />
+                    </Filters>
+                </Section>
+            </Main>
+        </>
     )
 }
