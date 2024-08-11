9ac897041fad8a3caee05a92416c7dd299d5bbe9
LaryssaGabi <lary.gabriela85@gmail.com>
2024-08-11 16:21:28 -0300

    styled

--- frontend/src/screens/home/index.tsx
+++ frontend/src/screens/home/index.tsx
@@ -1,4 +1,4 @@
-import { Balance, Filters, Header, InputGroup, Main, Section } from "./home-styles";
+import { Balance, ChartAction, ChartContainer, ChartContent, Filters, Header, InputGroup, Main, Section } from "./home-styles";
 import { Logo } from '../../components/logo/index'
 import { Button } from "../../components/button";
 import { Title } from "../../components/title";
@@ -24,29 +24,59 @@ export function Home() {
                         <InputGroup>
                             <InputMask
                                 component={Input}
-                                mask="dd/mm/yyyy"
-                                replacement={{ d: /\d/, m: /\d/, y: /\d/ }}
+                                mask="dd/mm/aaaa"
+                                replacement={{ d: /\d/, m: /\d/, a: /\d/ }}
                                 variant="dark"
                                 label="Início"
-                                placeholder="dd/mm/yyyy"
+                                placeholder="dd/mm/aaaa"
                             />
                             <InputMask
                                 component={Input}
-                                mask="dd/mm/yyyy"
-                                replacement={{ d: /\d/, m: /\d/, y: /\d/ }}
+                                mask="dd/mm/aaaa"
+                                replacement={{ d: /\d/, m: /\d/, a: /\d/ }}
                                 variant="dark"
                                 label="Fim"
-                                placeholder="dd/mm/yyyy"
+                                placeholder="dd/mm/aaaa"
                             />
                             <ButtonIcon />
                         </InputGroup>
                     </Filters>
-                    
+
                     <Balance>
-                        <Card title="Saldo" amount={1000000}/>
-                        <Card title="Saldo" amount={1000000} variant="incomes"/>
-                        <Card title="Saldo" amount={1000000} variant="expenses"/>
+                        <Card title="Saldo" amount={1000000} />
+                        <Card title="Saldo" amount={1000000} variant="incomes" />
+                        <Card title="Saldo" amount={1000000} variant="expenses" />
                     </Balance>
+
+                    <ChartContainer>
+                        <header>
+                            <Title title="Gastos" subtitle="Despesas por categoria no período" />
+                        </header>
+                        <ChartContent>
+
+                        </ChartContent>
+                    </ChartContainer>
+
+                    <ChartContainer>
+                        <header>
+                            <Title title="Evolução Financeira" subtitle="Saldo, Receitas e Gastos no ano" />
+
+                            <ChartAction>
+                                <InputMask
+                                    component={Input}
+                                    mask="aaaa"
+                                    replacement={{ a: /\d/ }}
+                                    variant="black"
+                                    label="Ano"
+                                    placeholder="aaaa"
+                                />
+                                <ButtonIcon />
+                            </ChartAction>
+                        </header>
+
+                        <ChartContent></ChartContent>
+                    </ChartContainer>
+
                 </Section>
             </Main>
         </>
